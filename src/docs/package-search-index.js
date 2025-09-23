043c2b4cb55d6c699f02653f661b7cb2180a30a7
GigBit Flyz <tolkachv_v@mail.ru>
2025-09-24 00:51:50 +0500

    doc

---
+++ src/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example.app"},{"l":"org.example.bot"}];updateSearchResults();
---
+++ src/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.bot","l":"LogicBot"},{"p":"org.example.app","l":"Main"},{"p":"org.example.bot","l":"TelegramBot"}];updateSearchResults();
